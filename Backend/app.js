b90d935f0e728ed636508b1a378e6f4e427cc98f
shubham <shubhampatil080699@gmail.com>
2023-08-14 22:06:16 +0530

    First Commit

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://shubhampatil:Shubham@cluster0.u0izivu.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
